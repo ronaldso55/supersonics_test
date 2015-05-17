9910a92940ed92bd7f64ff982ad062aa63eec56d
Ronald So <ronald.so@gmail.com>
2015-05-17 19:31:46 -0400

    Missed a file to demo i18n.

--- app/main/scripts/profile/ProfileController.js
+++ app/main/scripts/profile/ProfileController.js
@@ -10,6 +10,6 @@
             i18n = filter('translate');
         vm.navbarTitle = "Profile";
 //        vm.h3content = "Profile H3 content";
-        vm.h3content = i18n("GREETING");
+        vm.h3content = i18n("HEADLINE");
     }
 })();
