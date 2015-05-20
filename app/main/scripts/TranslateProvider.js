514c54080db87753f0567fc092a69710e4853e4e
Ronald So <ronald.so@gmail.com>
2015-05-20 00:49:41 -0400

    I18N hookup.  de.json not working yet.

--- app/main/scripts/TranslateProvider.js
+++ app/main/scripts/TranslateProvider.js
@@ -4,12 +4,21 @@
     angular.module('main').config(TranslateProvider);
 
     function TranslateProvider($translateProvider) {
-        $translateProvider.translations('en', {
-            HEADLINE : 'Hello there, This is my awesome app!', INTRO_TEXT : 'And it has i18n support!'
-        }).translations('de', {
-            HEADLINE : 'Hey, das ist meine großartige App!', INTRO_TEXT : 'Und sie untersützt mehrere Sprachen!'
+        console.log("Configuring translateProvider");
+//        $translateProvider.translations('en', {
+//            "profile": {
+//                "HEADLINE" : 'Hello there, This is my awesome app!', "INTRO_TEXT" : 'And it has i18n support!'
+//            }
+//        }).translations('de', {
+//            "profile": {
+//                "HEADLINE" : 'Hey, das ist meine großartige App!', "INTRO_TEXT" : 'Und sie untersützt mehrere Sprachen!'
+//            }
+//        });
+//        $translateProvider.useLocalStorage();
+        $translateProvider.useStaticFilesLoader({
+            prefix : '/l10n/',
+            suffix : '.json'
         });
-        $translateProvider.preferredLanguage('en');
-        $translateProvider.useLocalStorage();
+        $translateProvider.preferredLanguage('en_US');
     }
 })();
--- app/main/scripts/profile/ProfileController.js
+++ app/main/scripts/profile/ProfileController.js
@@ -9,7 +9,5 @@
         var vm = this,
             i18n = filter('translate');
         vm.navbarTitle = "Profile";
-//        vm.h3content = "Profile H3 content";
-        vm.h3content = i18n("HEADLINE");
     }
 })();
